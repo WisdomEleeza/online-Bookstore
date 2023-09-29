3843932e18e64b3aac4c8a906f81607fa615cc8b
Wisdom Eleeza <whiy07@gmail.com>
2023-09-29 13:38:41 +0000

    configuring and importing packages into the project files

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { Application } from "express";
+import compression from "compression";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+
+class App {
+  public express: Application;
+  public port: number
+}
