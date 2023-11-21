c4447cf11f5867a1aefed7d109e4a776319b7234
Wisdom Eleeza <whiy07@gmail.com>
2023-11-21 15:29:00 +0000

    update users jsonwebtoken

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -8,9 +8,6 @@ dotenv.config();
 
 // Define a function that creates a JWT for a given user
 export const createToken = (user: User): string => {
-  // Use jwt.sign to create a JWT with the user's ID as the payload
-  // process.env.JWT_SECRET is the secret key for signing the JWT, loaded from the environment variables
-  // expiresIn: "1d" sets the expiration time of the token to 1 day
   return jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, {
     expiresIn: "1d",
   });
