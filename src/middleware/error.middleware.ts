7d871ba64d2974787d50543c7d4d0625f1055365
Wisdom Eleeza <whiy07@gmail.com>
2023-11-22 11:27:19 +0000

    add comment to errorMiddleware

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -5,15 +5,15 @@ function errorMiddleware(
   error: HttpException,
   req: Request,
   res: Response,
-  // _next: NextFunction,
 ): void {
-  const status = error.status || 500;
-  const message = error.message || "Something went wrong";
+  const status = error.status || 500; // Use the status from HttpException or default to 500
+  const message = error.message || "Something went wrong"; // Use the message from HttpException or default to "Something went wrong"
+
+  // Set the HTTP status and send a JSON response with status and message
   res.status(status).send({
     status,
     message,
   });
 }
-
 
 export default errorMiddleware;
