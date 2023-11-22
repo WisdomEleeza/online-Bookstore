f988ac39657088a6ea985c7c9211da9c321b6037
Wisdom Eleeza <whiy07@gmail.com>
2023-11-22 09:44:25 +0000

    add login validation, add .vscode to gitignore, and update schema.prisma model

--- src/resources/users/user.validate.ts
+++ src/resources/users/user.validate.ts
@@ -1,8 +1,17 @@
 import Joi from "joi";
 
 const register = Joi.object({
+  name: Joi.string().max(255),
+
   email: Joi.string().max(255).required(),
+
   password: Joi.string().min(8).required(),
 });
 
-module.exports = { register };
+const login = Joi.object({
+  email: Joi.string().max(255).required(),
+
+  password: Joi.string().min(8).required(),
+});
+
+module.exports = { register, login };
