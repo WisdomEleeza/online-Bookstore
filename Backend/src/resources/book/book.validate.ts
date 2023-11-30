81506bf2910ffc52c80da70ba9302e406db97674
Wisdom Eleeza <whiy07@gmail.com>
2023-11-30 17:24:48 +0000

    validate book schema data types using Joi package or module

--- Backend/src/resources/book/book.validate.ts
+++ Backend/src/resources/book/book.validate.ts
@@ -3,7 +3,15 @@ import Joi from "joi";
 const Book = Joi.object({
   title: Joi.string().max(255).required(),
 
-  
+  author: Joi.string().max(255).required(),
+
+  ISBN: Joi.string().max(255).required(),
+
+  genre: Joi.string().max(255).required(),
+
+  price: Joi.string().max(255).required(),
+
+  quantityAvailable: Joi.string().max(255).required(),
 });
 
 export default { Book };
