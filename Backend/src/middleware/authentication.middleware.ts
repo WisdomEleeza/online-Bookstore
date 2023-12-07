f5497a605e73c69fe9ad9b587730c1f774bf878a
Wisdom Eleeza <whiy07@gmail.com>
2023-12-07 23:21:19 +0000

    Remove console.log(4)

--- Backend/src/middleware/authentication.middleware.ts
+++ Backend/src/middleware/authentication.middleware.ts
@@ -22,34 +22,24 @@ async function authenticatedMiddleware(
     const payload = token.verifyToken(accessToken);
 
     if (payload instanceof JsonWebTokenError) {
-      // console.log("Unauthorized: Token verification failed");
       return next(new HttpException(401, "Unauthorized"));
     }
 
     // Check if payload is not undefined and has the expected structure
     if (payload && typeof payload === "object" && "id" in payload) {
       const userId = payload.id;
-      // console.log("Fetching user from database for user id:", userId);
 
       const user = await prisma.user.findUnique({
         where: { id: userId },
       });
 
-      // console.log("user", user);
-
       if (!user) return next(new HttpException(401, "Unauthorized"));
-      // console.log("Unauthorized: User not found in the database");
 
-      // console.log("User found. Proceeding to the next middleware");
       return next();
     } else {
-      // console.log(
-      //   "Unauthorized: Token payload does not have the expected structure",
-      // );
       return next(new HttpException(401, "Unauthorized"));
     }
   } catch (error) {
-    // console.error("Token verification error", error);
     return next(new HttpException(401, "Unauthorized"));
   }
 }
